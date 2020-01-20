f77ea86bbc0432b0666825510d442bf293732ee9
Egor Vasyakin <evasyakin@mail.ru>
2020-01-20 21:42:05 +0700

    Create Response.php

---
+++ src/Response.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package evas-php/evas-web
+ */
+namespace Evas\Web;
+
+use Evas\Http\Response as HttpResponse;
+use Evas\Web\App;
+
+/**
+ * Класс ответа веб-приложения.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Response extends HttpResponse
+{
+    /**
+     * Переопределение отправки ответа.
+     * @param int|null код статуса
+     * @param string|null тело
+     * @param array|null заголовки
+     */
+    public function send(int $code = null, string $body = null, array $headers = null)
+    {
+        parent::send($code, $body, $headers);
+        $this->applyHeaders();
+        echo $this->getBody();
+    }
+
+    /**
+     * Применение установленных заголовков.
+     * @return self
+     */
+    public function applyHeaders()
+    {
+        header("HTTP/1.1 $this->statusCode $this->statusText");
+        foreach ($this->headers as $name => $value) {
+            header("$name: $value");
+        }
+        return $this;
+    }
+
+    /**
+     * Переопределение редиректа для подстановки uri приложения.
+     * @param string куда
+     */
+    public function redirect(string $to)
+    {
+        if (strpos($to, 'http') !== 0) {
+            $to = App::getUri() . $to;
+        }
+        return parent::redirect($to);
+    }
+}
