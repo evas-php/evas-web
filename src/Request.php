e3a24942062f274d3e887aded1bb14fc30cd8f45
Egor Vasyakin <evasyakin@mail.ru>
2020-01-20 21:41:56 +0700

    Create Request.php

---
+++ src/Request.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package evas-php/evas-web
+ */
+namespace Evas\Web;
+
+use Evas\Http\Request as HttpRequest;
+
+/**
+ * Класс запроса веб-приложения.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Request extends HttpRequest
+{
+    /**
+     * Конструктор.
+     */
+    public function __construct()
+    {
+        $this
+            ->withMethod($_SERVER['REQUEST_METHOD'])
+            ->withUri($_SERVER['REQUEST_URI'])
+            ->withHeaders($this->getAllHeaders())
+            ->withUserIp($_SERVER['REMOTE_ADDR'])
+            ->withPost($_POST)
+            ->withQuery($_GET)
+            ->withBody(file_get_contents('php://input'));
+    }
+
+    /**
+     * Получение заголовков запроса из getallheaders или $_SERVER.
+     * @return array
+     */
+    public function getAllHeaders(): array
+    {
+        if (function_exists('getallheaders')) {
+            return getallheaders();
+        }
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (preg_match('/^HTTP_(.+)$/', $name, $matches)) {
+                $headers[$matches[1]] = $value;
+            }
+        }
+        return $headers;
+    }
+}
